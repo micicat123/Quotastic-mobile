2667c6dde7682c1205b22cc8a54f7331dec94913
Micicat123 <mici.zep@gmail.com>
2023-10-12 19:43:45 +0200

    fixes

--- app/settings/index.tsx
+++ app/settings/index.tsx
@@ -1,10 +1,15 @@
 import { View, Text } from 'react-native';
-import LoggedInProps from '../../common/interface';
+import { Theme, customStyles } from '../../config/theme.config';
 
 export default function SettingsScreen() {
   return (
     <View>
-      <Text>Settings</Text>
+      <Text style={[customStyles.h4, { color: Theme.lightColors.primary }]}>
+        Profile <Text style={customStyles.h4}>Settings</Text>
+      </Text>
+      <Text>Change your contact information</Text>
+      <Text>Change your password</Text>
+      <Text>Change your profile picture</Text>
     </View>
   );
 }
